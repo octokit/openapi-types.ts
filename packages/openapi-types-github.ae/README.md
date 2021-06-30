609e0305389a55082a8e32597dab4efc93b49306
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-29 17:49:28 -0700

    fix: add README to new packages and correct repository URL

---
+++ packages/openapi-types-github.ae/README.md
@@ -0,0 +1,17 @@
+# @octokit/openapi-types-github.ae
+
+> Generated TypeScript definitions based on GitHub's OpenAPI spec for github.ae
+
+This package is continously updated based on [GitHub's OpenAPI specification](https://github.com/github/rest-api-description/)
+
+## Usage
+
+```ts
+import { components } from "@octokit/openapi-types-github.ae";
+
+type Repository = components["schemas"]["full-repository"];
+```
+
+## License
+
+[MIT](LICENSE)
--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -46,12 +46,39 @@ async function run() {
           description: `Generated TypeScript definitions based on GitHub's OpenAPI spec for ${name}`,
           repository: {
             type: "git",
-            url: "https://github.com/octokit/openapi-types.git",
+            url: "https://github.com/octokit/openapi-types.ts.git",
             directory: `packages/${packageName}`,
           },
           ...packageDefaults,
         }),
         { parser: "json" }
+      )
+    );
+    await writeFile(
+      `packages/${packageName}/README.md`,
+      prettier.format(
+        `
+# @octokit/${packageName}
+
+> Generated TypeScript definitions based on GitHub's OpenAPI spec ${
+          packageName === "openapi-types" ? "" : `for ${name}`
+        }
+
+This package is continously updated based on [GitHub's OpenAPI specification](https://github.com/github/rest-api-description/) 
+
+## Usage
+
+\`\`\`ts
+import { components } from "@octokit/${packageName}";
+
+type Repository = components["schemas"]["full-repository"]
+\`\`\`
+
+## License
+
+[MIT](LICENSE)
+`,
+        { parser: "markdown" }
       )
     );
 
