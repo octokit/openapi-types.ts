910594f21fd74be5166e68368d5435f8ee03a118
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-06-29 17:39:18 -0700

    feat: publish separate openapi-types packages for GHAE and GHES: `@octokit/openapi-types-ghes-2.22`, `@octokit/openapi-types-ghes-3.0`, `@octokit/openapi-types-ghes-3.1`, `@octokit/openapi-types-github.ae` (#110)

--- scripts/download.js
+++ scripts/download.js
@@ -1,20 +1,67 @@
 const { get } = require("https");
-const fs = require("fs");
+const { createWriteStream } = require("fs");
+const { mkdir, rm } = require("fs/promises");
+
+const { Octokit } = require("@octokit/core");
+const { getCurrentVersions } = require("github-enterprise-server-versions");
 
 if (!process.env.OCTOKIT_OPENAPI_VERSION) {
   throw new Error("OCTOKIT_OPENAPI_VERSION is not set");
 }
 
-download(process.env.OCTOKIT_OPENAPI_VERSION.replace(/^v/, "")).then(
+run(process.env.OCTOKIT_OPENAPI_VERSION.replace(/^v/, "")).then(
   () => console.log("done"),
   console.error
 );
 
-function download(version) {
-  const path = `cache/openapi-schema.json`;
-  const url = `https://unpkg.com/@octokit/openapi@${version}/generated/api.github.com.json`;
+async function run(version) {
+  await rm("cache", { recursive: true });
+  await mkdir("cache");
 
-  const file = fs.createWriteStream(path);
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+
+  const { data } = await octokit.request(
+    "GET /repos/{owner}/{repo}/contents/{path}",
+    {
+      owner: "octokit",
+      repo: "openapi",
+      path: "generated",
+      ref: "main",
+    }
+  );
+
+  if (!Array.isArray(data)) {
+    throw new Error(
+      "https://github.com/octokit/openapi/tree/main/generated is not a directory"
+    );
+  }
+
+  const currentGHESVersions = await getCurrentVersions();
+  for (const file of data) {
+    if (!/\.json$/.test(file.name)) continue;
+    if (/deref/.test(file.name)) continue;
+
+    if (/^ghes-/.test(file.name)) {
+      if (
+        !currentGHESVersions.includes(
+          parseFloat(file.name.substr("ghes-".length))
+        )
+      ) {
+        continue;
+      }
+    }
+
+    download(version, file.name);
+  }
+}
+
+function download(version, fileName) {
+  const localPath = `cache/${fileName}`;
+
+  const file = createWriteStream(localPath);
+  const url = `https://unpkg.com/@octokit/openapi@${version}/generated/${fileName}`;
 
   console.log("Downloading %s", url);
 
@@ -25,7 +72,7 @@ function download(version) {
         .on("finish", () =>
           file.close((error) => {
             if (error) return reject(error);
-            console.log("%s written", path);
+            console.log("%s written", localPath);
             resolve();
           })
         )
---
+++ scripts/generate-types.js
@@ -0,0 +1,64 @@
+const { readdir, mkdir, rm, writeFile } = require("fs/promises");
+const { basename } = require("path");
+
+const prettier = require("prettier");
+const openapiTS = require("openapi-typescript").default;
+
+if (!process.env.OCTOKIT_OPENAPI_VERSION) {
+  throw new Error("OCTOKIT_OPENAPI_VERSION is not set");
+}
+
+run();
+
+const packageDefaults = {
+  publishConfig: {
+    access: "public",
+  },
+  version: "0.0.0-development",
+  main: "",
+  types: "types.d.ts",
+  author: "Gregor Martynus (https://twitter.com/gr2m)",
+  license: "MIT",
+  octokit: {
+    "openapi-version": process.env.OCTOKIT_OPENAPI_VERSION.replace(/^v/, ""),
+  },
+};
+
+async function run() {
+  await rm("packages", { recursive: true }).catch(() => {});
+  await mkdir("packages");
+
+  const files = await readdir("cache");
+  for (const fileName of files) {
+    if (!/\.json$/.test(fileName)) continue;
+
+    const name = basename(fileName, ".json");
+
+    const packageName =
+      name === "api.github.com" ? "openapi-types" : `openapi-types-${name}`;
+
+    await mkdir(`packages/${packageName}`);
+    await writeFile(
+      `packages/${packageName}/package.json`,
+      prettier.format(
+        JSON.stringify({
+          name: `@octokit/${packageName}`,
+          description: `Generated TypeScript definitions based on GitHub's OpenAPI spec for ${name}`,
+          repository: {
+            type: "git",
+            url: "https://github.com/octokit/openapi-types.git",
+            directory: `packages/${packageName}`,
+          },
+          ...packageDefaults,
+        }),
+        { parser: "json" }
+      )
+    );
+
+    await writeFile(
+      `packages/${packageName}/types.ts`,
+      await openapiTS(`cache/${name}.json`)
+    );
+    console.log(`packages/${packageName}/types.ts written`);
+  }
+}
