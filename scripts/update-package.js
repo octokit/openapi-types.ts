b794a305e7a320741f06ba1cb5fe7cda721d0daf
wolfy1339 <webmaster@wolfy1339.com>
2022-07-14 15:53:54 -0400

    build(update-package): fix prettier

--- scripts/update-package.js
+++ scripts/update-package.js
@@ -1,4 +1,5 @@
 import { writeFile, readdir, readFile } from "node:fs/promises";
+import prettier from "prettier";
 
 if (!process.env.OCTOKIT_OPENAPI_VERSION) {
   throw new Error("OCTOKIT_OPENAPI_VERSION is not set");
@@ -30,6 +31,6 @@ async function updatePackage() {
 
   await writeFile(
     "package.json",
-    prettier(JSON.stringify(pkg, null), { parser: "json" })
+    prettier.format(JSON.stringify(pkg, null), { parser: "json" })
   );
 }
